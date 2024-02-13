ff02bbc584a4c099082057eeb229fadb009acfe0
elroicarmel <elroica277@gmail.com>
2024-02-13 22:52:34 +0200

    adding factory design pattern

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -3,19 +3,14 @@ from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
 class SocialNetwork:
     __instance = None
-    @staticmethod
-    def get_instance(name):
-        if SocialNetwork is None:
-            SocialNetwork(name)
-        return SocialNetwork.__instance
+    def __new__(cls, name):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
     def __init__(self, name):
-        if SocialNetwork.__instance is not None:
-            raise Exception("SocialNetwork is a Singleton. Use get_instance() method!")
-        else:
-            SocialNetwork.__instance = self
-            self.__name = name
-            self.__users = []
-            print(f"The social network {self.__name} was created!")
+        self.__name = name
+        self.__users = []
+        print(f"The social network {self.__name} was created!")
     def sign_up(self, user_name, password):
         if len(password) < 4 or len(password) > 8:
             raise Exception("The password must be between 4 and 8 characters.")
@@ -41,6 +36,7 @@ class SocialNetwork:
         for user in self.__users:
             string += f"{user}\n"
         return string
+
 class User:
     def __init__(self, user_name: str, password: str):
         self.__user_name = user_name
@@ -56,17 +52,18 @@ class User:
     def is_logged_in(self) -> bool:
         return self.__logged_in
     def follow(self, other):
-        check_if_user_logged_in(self)
+        self.check_if_user_logged_in()
         if self not in other.__followers:
             other.__followers.append(self)
             print(f"{self.__user_name} started following {other.get_name()}")
         else:
             print(f"{self.__user_name} already follows {other.get_name()}")
     def unfollow(self, other):
-        check_if_user_logged_in(self)
+        self.check_if_user_logged_in()
         if self in other.__followers:
             other.__followers.remove(self)
             print(f"{self.__user_name} unfollowed {other.get_name()}")
+
     def add_notification(self, message: str, flag: bool = True):
         if flag:
             print(f"notification to {self.__user_name}: {message}", end='')
@@ -78,27 +75,27 @@ class User:
         self.__logged_in = True
 
     def publish_post(self, post_type: str, text, price: int = None, location: str = None):
-        if post_type not in ['Text', 'Image', 'Sale']:
-            raise Exception("Invalid post type. Must be Text/Image/Sale")
-        check_if_user_logged_in(self)
-        post = None
-        if post_type == "Text":
-            post = TextPost(self, text)
-        elif post_type == "Image":
-            post = ImagePost(self, text)
-        elif post_type == "Sale":
-            post = SalePost(self, text, price, location)
+        self.check_if_user_logged_in()
+        post_builder = PostFactory()
+        post = post_builder.create(self, post_type, text, price, location)
         for user in self.__followers: # Notify the followers
             user.add_notification(f"{self.__user_name} has a new post", False)
         self.__posts_count+=1
         print(post)
         return post
+
+    def check_if_user_logged_in(self):
+        if self.is_logged_in() == False:
+            raise Exception(f"Error! {self.get_name()} is not logged in!")
+
     def print_notifications(self):
         print(f"{self.__user_name}'s notifications:")
         for notif in self.__notif:
             print(f"{notif}")
     def __str__(self):
         return f"User name: {self.__user_name}, Number of posts: {self.__posts_count}, Number of followers: {len(self.__followers)}"
+
+
 class Post(ABC):
     @abstractmethod
     def __init__(self, owner: User):
@@ -106,15 +103,16 @@ class Post(ABC):
     def get_owner(self):
         return self.__owner
     def like(self, user: User):
-        check_if_user_logged_in(user)
+        user.check_if_user_logged_in()
         if user != self.__owner:
             self.__owner.add_notification(f"{user.get_name()} liked your post")
             print()
     def comment(self, user: User, text: str):
-        check_if_user_logged_in(user)
+        user.check_if_user_logged_in()
         if user != self.__owner:
             self.__owner.add_notification(f"{user.get_name()} commented on your post")
             print(f": {text}")
+
 class TextPost(Post):
     def __init__(self, owner: User, content):
         super().__init__(owner)
@@ -122,17 +120,19 @@ class TextPost(Post):
     def __str__(self):
         return (f"{self.get_owner().get_name()} published a post:\n"
                     f"\"{self.__content}\"\n")
+
 class ImagePost(Post):
     def __init__(self, owner: User, image_path):
         super().__init__(owner)
         self.__image_path = image_path
     def display(self):
-        # image = mpimg.imread(self.__image_path)
-        # plt.imshow(image)
-        # plt.show()
+        image = mpimg.imread(self.__image_path)
+        plt.imshow(image)
+        plt.show()
         print("Shows picture")
     def __str__(self):
         return f"{self.get_owner().get_name()} posted a picture\n"
+
 class SalePost(Post):
     def __init__(self, owner: User, item: str, price: int, location: str):
         super().__init__(owner)
@@ -156,6 +156,22 @@ class SalePost(Post):
         status = "Sold!" if self.__sold else "For sale!"
         return (f"{self.get_owner().get_name()} posted a product for sale:\n"
                 f"{status} {self.__item_name}, price: {self.__price}, pickup from: {self.__location}\n")
-def check_if_user_logged_in(user: User):
-    if (user.is_logged_in() == False):
-        raise Exception(f"Error! {user.get_name()} is not logged in!")
+
+class PostFactory:
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+    def create(self, owner: User, post_type: str, text, price: int = None, location: str = None):
+        post = None
+        if post_type == "Text":
+            post = TextPost(owner, text)
+        elif post_type == "Image":
+            post = ImagePost(owner, text)
+        elif post_type == "Sale":
+            post = SalePost(owner, text, price, location)
+        else:
+            raise Exception("Invalid post type. Must be Text/Image/Sale")
+        return post
